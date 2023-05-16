ca4778054d4d9076a54697746c2d3a7154513cd8
Cátia Antunes <catiafantunes@gmail.com>
2023-05-16 14:23:20 +0100

    Update database_v1.sql
    Correções ao ficheiro database_v1. Inclusão da coluna "name" na tabela "songs"

--- server/db/database_v1.sql
+++ server/db/database_v1.sql
@@ -2,35 +2,36 @@ CREATE SCHEMA IF NOT EXISTS `adonis`;
 use `adonis`;
 
 CREATE TABLE IF NOT EXISTS `users` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
+  `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT,
   `username` VARCHAR(80) NOT NULL UNIQUE,
   `email` VARCHAR(254) NOT NULL UNIQUE,
   `password` VARCHAR(60) NOT NULL,
-  `created_at` DATETIME2,
-  `updated_at` DATETIME2
+  `created_at` DATETIME,
+  `updated_at` DATETIME,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB;
+)ENGINE=InnoDB;
 
 CREATE TABLE IF NOT EXISTS `playlists` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
+  `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT,
   `title` VARCHAR(255),
-  `user_id` INT UNSIGNED,
+  `user_id` bigint UNSIGNED,
   FOREIGN KEY (`user_id`) REFERENCES users(`id`),
-  `created_at` DATETIME2,
-  `updated_at` DATETIME2,
+  `created_at` DATETIME,
+  `updated_at` DATETIME,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB;
+)ENGINE=InnoDB;
 
 CREATE TABLE IF NOT EXISTS `songs` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
+  `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(255),
   `artist` VARCHAR(255),
   `listened` BOOLEAN,
-  `playlist_id` INT UNSIGNED,
+  `playlist_id` bigint UNSIGNED,
   FOREIGN KEY (`playlist_id`) REFERENCES playlists(`id`),
-  `created_at` DATETIME2,
-  `updated_at` DATETIME2,
+  `created_at` DATETIME,
+  `updated_at` DATETIME,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB;
+)ENGINE=InnoDB;
 
 insert into adonis.users (username, email, password, created_at, updated_at) values 
 ("john.doe@example.com","john.doe@example.com", "$2a$10$N5/NPl/L3W35zj0NNIPmw.xwosgE41ytw2QfaCswoFm/UQyO8UaSi", sysdate(), sysdate()),
@@ -47,7 +48,7 @@ insert into adonis.playlists (title, user_id, created_at, updated_at) values
 ("90s Nostalgia", 2, sysdate(), sysdate())
 ;
 
-insert into adonis.songs (artist, name, playlist_id, created_at, updated_at) values
+insert into adonis.songs (artist, `name`, playlist_id, created_at, updated_at) values
 ("Enya", "Only Time", 1, sysdate(), sysdate()),
 ("Ludovico Einaudi", "Nuvole Bianche", 1, sysdate(), sysdate()),
 ("Sigur Rós", "Untitled #3 (Samskeyti)", 1, sysdate(), sysdate()),
